407b8cce76c0f164e3b5d1c7bba387881a511eef
Bio121 <daniel.alejandro121@gmail.com>
2021-11-20 03:45:17 -0600

    aut0 back
    aut0

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const user_controller = require("../controllers/userController");
+const checkJwt = require("../middleware/checkJwt");
 //indicar
 //-metodo HTTP
 //-ruta
 //-funcion
 
-router.post("/user", user_controller.user_create);
-router.get("/user", user_controller.user_getall);
+router.post("/user", checkJwt, user_controller.user_create);
+router.get("/user", checkJwt, user_controller.user_getall);
 router.get("/user/:id", user_controller.user_getbyid);
 router.put("/user/:id", user_controller.user_update);
 router.delete("/user/:id", user_controller.user_delete);
